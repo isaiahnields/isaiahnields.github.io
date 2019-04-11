272aa72dbbbecaa799f8c6ae7f6cf3517e521958
Isaiah Nields <imnields@gmail.com>
2019-04-11 00:26:32 -0400

    Updates

---
+++ precache-manifest.bc5e4643b45c811c8f274b6b23b25a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "457c3b779b53dbc8b369",
+    "url": "/static/js/main.59ae5a15.chunk.js"
+  },
+  {
+    "revision": "0af663504a51351fafc9",
+    "url": "/static/js/2.4a9d0898.chunk.js"
+  },
+  {
+    "revision": "457c3b779b53dbc8b369",
+    "url": "/static/css/main.2b1a8dbf.chunk.css"
+  },
+  {
+    "revision": "4eaac68dc4b659910de407f6cac642bd",
+    "url": "/index.html"
+  }
+];
